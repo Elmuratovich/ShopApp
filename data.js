49f97b6419eab41e4c1e971c74878ab695904987
Makhmud <makhmudkuzikaev@gmail.com>
2023-09-28 13:06:33 +0600

    start project

---
+++ data.js
@@ -0,0 +1,21 @@
+module.exports = function() {
+    return {
+        products: [
+            {id: 1, name: "Samsung S5", price: 1000, imageUrl: '1.jpg', description: 'idare eder', category: 'Telefon' },
+            {id: 2, name: "Samsung S6", price: 2000, imageUrl: '2.jpg', description: 'iyi telefon', category: 'Telefon' },
+            {id: 3, name: "Samsung S7", price: 3000, imageUrl: '3.jpg', description: 'guzel', category: 'Telefon' },
+            {id: 4, name: "Samsung S8", price: 4000, imageUrl: '4.jpg', description: 'pahali', category: 'Telefon' },
+            {id: 5, name: "Samsung S9", price: 5000, imageUrl: '5.jpg', description: 'ucuz', category: 'Telefon' },
+            {id: 6, name: "Samsung S10", price: 6000, imageUrl: '6.jpg', description: 'kullanisli', category: 'Bilgisayar' },
+            {id: 7, name: "Samsung S11", price: 7000, imageUrl: '7.jpg', description: 'super telefon', category: 'Bilgisayar' },
+        ],
+        categories: [
+            {id: 1, name: "Telefon"},
+            {id: 2, name: "Bilgisayar"},
+            {id: 3, name: "Elektronik"}
+        ],
+        orders:[
+
+        ]
+    }
+}
---
+++ src/app/model/product.repository.ts
@@ -0,0 +1,33 @@
+import { Injectable, OnInit } from "@angular/core";
+import { Product } from "./product.model";
+import { RestService } from "./rest.service";
+import { Category } from "./category.model";
+
+@Injectable()
+export class ProductRepository implements OnInit {
+    private products: Product[] = [];
+
+    constructor(private restService: RestService){
+        this.restService
+        .getProducts()
+        .subscribe(products => this.products = products);
+    }
+
+    ngOnInit(): void {
+      
+        }
+
+    getProduct(id: number): Product | undefined{
+        return this.products.find(i => i.id === id);
+    }
+
+    getProducts(category: Category | null | undefined = undefined): Product[] {
+        if(category){
+            return this.products.filter(p => p.category == category.name);
+        }
+        else{
+            return this.products;
+        }
+    }
+
+}
